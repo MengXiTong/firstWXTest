c99012f3f0018886eeb3ce584782c6491919ea28
MengXiTong <1317545539@qq.com>
2018-08-02 10:13:26 +0800

    修改

--- firstWXTest/pages/userInfo/userInfo.js
+++ firstWXTest/pages/userInfo/userInfo.js
@@ -3,19 +3,37 @@ const app = getApp()
 
 Page({
   data: {
-    message:'俞健',
+    message: '俞健',
     userInfo: {},
     hasUserInfo: false
   },
-  onLoad: function () {
+  onLoad: function() {
     console.log("大家好");
+    this.getLocation();
   },
-  getUserInfo: function (e) {
+  getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  getLocation: () => {
+    // wx.getLocation({
+    //   type: 'wgs84',
+    //   success: (res) => {
+    //     this.data.latitude = res.latitude;
+    //     this.data.longitude = res.longitude;
+    //     console.log(this);
+    //   }
+    // })
+  },
+  takeEncode: ()=>{
+    wx.scanCode({
+      success: (res) => {
+        console.log(res);
+      }
     })
   }
 })
